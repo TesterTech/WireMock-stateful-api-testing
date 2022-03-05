70e344752d784a0ec82fdfcb73363020b416cc85
Eric de Graaf <egraaf@thinkpad.local>
2022-03-05 22:36:51 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# WireMock-statefull-api-testing
-WireMock and Rest-Assured for statefull API testing
+# WireMock-stateful-api-testing
+WireMock and Rest-Assured for stateful API testing
+
+## WireMock standalone
+This script makes use of wiremock standalone jar
+- To record start WireMock standalone the following way
+```shell
+java -jar wiremock-jre8-standalone-<version>.jar --port <7777> \
+  --proxy-all="http://<proxy address>:<potrt>" \
+  --record-mappings \ 
+  --verbose 
+```
+
+## Mappings
+- Make the mapping by putting your request/responsemappings in the mappings.json file in the mappings dir
+- Remember to restart once new mappings have been added. 
+- The mappings used in the video [can be found here](./mapping-example.json)
+
+## State
+- State can be setup in the mappings, can be called using ```__admin/scenarios```
+
+## Rest Client (desktop)
+- Using [insomnia rest client](https://insomnia.rest/) as a rest client
+- This is mainly used to setup and play around with the Backend JAVA API
+
+## Application
+- Todo Backend applications [more info here](https://todobackend.com/) 
+- The specific one I used was [this one] which used Javalin (https://github.com/johanvogelzang/todo-backend)
+
+# References 
+- https://wiremock.org/docs/stateful-behaviour/
+- https://wiremock.org/docs/record-playback/
+- 
--- src/test/java/nl/testertech/wiremock/ScenarioStateTest.java
+++ src/test/java/nl/testertech/wiremock/ScenarioStateTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.Map.Entry;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,7 +16,8 @@ import static io.restassured.RestAssured.given;
 public class ScenarioStateTest {
 
     public static final String URI = "http://127.0.0.1";
-    private RequestSpecification requestSpec;
+    private static RequestSpecification requestSpec;
+    private static LinkedHashMap<Object, Object> scenarioMapWithState;
 
     @BeforeEach
     public void createRequestSpec() {
@@ -23,36 +25,52 @@ public class ScenarioStateTest {
                 setBaseUri(URI).
                 setPort(7777).
                 build();
+        cleanTheStateRestcall();
     }
-
-    @Test
-    public void setUpScenario() {
-        Assertions.assertEquals("Yes! Mock is working",
-                String.valueOf(given().spec(requestSpec).
-                when().get("/info").
-                then().assertThat().extract().body().asString()));
-    }
-
     @Test
     public void makeACleanState() {
-        Assertions.assertEquals("Cleaned the state",
-                String.valueOf(given().spec(requestSpec).
-                        when().delete("/").
-                        then().assertThat().extract().body().asString()));
-
-        Map<String, String> scenarioMaps = given().spec(requestSpec).
-                when().get("/__admin/scenarios").
-                then().assertThat().extract().jsonPath().getMap("$");
-
-        Set<Map.Entry<String, String>> entrySet = scenarioMaps.entrySet();
-        ArrayList<Object> arrayList = new ArrayList<>();
-        Map.Entry o = (Map.Entry)entrySet.toArray()[0];
-        arrayList.addAll((Collection<?>) o.getValue());
-        LinkedHashMap<Object, Object> scenarioMap = new LinkedHashMap<>();
-        for(int i = 0; i<arrayList.size();i++){
-            scenarioMap.putAll((LinkedHashMap)arrayList.get(i));
-        }
-        Assertions.assertTrue(scenarioMaps.containsKey("scenarios"));
-        Assertions.assertEquals("clean state", scenarioMap.get("state"));
+        loadScenarioStateFromRest();
+        Assertions.assertEquals("clean state", scenarioMapWithState.get("state"));
     }
+    @Test
+    public void cleanStateGivesNoItems() {
+        Assertions.assertEquals("[]",
+                given().spec(requestSpec).
+                        when().get("/").
+                        then().extract().body().asString()
+        );
+    }
+    @Test
+    public void newItemGivesAFilledState() {
+        final String requestBody = "{\"title\": \"todo-todo ... todo, todo-todo-todo.... \"}";
+        given().spec(requestSpec).
+                request().body(requestBody).
+                when().post("/").
+                then().extract().body().asString();
+        loadScenarioStateFromRest();
+        Assertions.assertEquals("filled state", scenarioMapWithState.get("state"));
+    }
+    private void cleanTheStateRestcall() {
+        Assertions.assertEquals("Cleaned the state",
+                given().spec(requestSpec).
+                        when().delete("/").
+                        then().extract().body().asString()
+        );
+    }
+    private static void loadScenarioStateFromRest() {
+        Map<String, String> scenarioMaps =
+                given().spec(requestSpec).
+                        when().get("/__admin/scenarios").
+                        then().assertThat().extract().jsonPath().getMap("$");
+        scenarioMapWithState = getScenarioMap(scenarioMaps);
+    }
+    private static LinkedHashMap<Object, Object> getScenarioMap(Map<String, String> scenarioMaps) {
+        Set<Entry<String, String>> entrySet = scenarioMaps.entrySet();
+        var o = (Entry) entrySet.toArray()[0];
+        ArrayList<Object> arrayList = new ArrayList<>((Collection<?>) o.getValue());
+        LinkedHashMap<Object, Object> scenarioMap = new LinkedHashMap<>();
+        for (Object value : arrayList) scenarioMap.putAll((LinkedHashMap) value);
+        return scenarioMap;
+    }
+
 }
